45165d87c49ae8431965461e17dd65998e825814
onurmacit <onurmaciit@gmail.com>
2025-02-12 16:25:55 +0300

    Improved service startup and added conditional support for web and celery
    - Replaced `nc` with `wait-for-it.sh` for more reliable database startup check.
    - Added conditional logic to run migrations and start the web server when `web` is passed as an argument.
    - Added conditional support for starting a celery worker when `celery` is passed as an argument.
    - Included error handling for invalid arguments.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,18 @@
 set -e
 
 echo "Waiting for database..."
-while ! nc -z db 5432; do
-  sleep 1
-done
-echo "Database initialized!"
+./wait-for-it.sh db:5432 --timeout=30 --strict -- echo "Database is up!"
 
-python manage.py migrate
-
-exec python manage.py runserver 0.0.0.0:8000
+if [ "$1" = "web" ]; then
+    echo "Running migrations..."
+    python manage.py migrate movies
+    python manage.py migrate
+    echo "Starting web server..."
+    exec python manage.py runserver 0.0.0.0:8000
+elif [ "$1" = "celery" ]; then
+    echo "Starting celery worker..."
+    exec celery -A imdb_clone.celery.app worker --loglevel=info
+else
+    echo "Invalid argument provided. Use 'web' or 'celery'."
+    exit 1
+fi
