c3d0506074b2fd8412e09c4a52607eb920124408
onurmacit <onurmaciit@gmail.com>
2025-02-23 01:46:05 +0300

    Update database creation in CI
    - Switch to createdb command
    - Standardize test database name
    - Add fallback for existing database

--- imdb_clone/test_settings.py
+++ imdb_clone/test_settings.py
@@ -7,13 +7,13 @@ SECRET_KEY = "django-test-key-123-for-ci"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "test_imdb",
+        "NAME": "test_db",
         "USER": "postgres",
         "PASSWORD": "postgres",
         "HOST": "localhost",
         "PORT": "5432",
         "TEST": {
-            "NAME": "test_imdb",
+            "NAME": "test_db",
         },
     }
 }
