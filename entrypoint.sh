2032eae0b65ead30864e56942e8b5cf57a0e4ec0
onurmacit <onurmaciit@gmail.com>
2025-02-23 23:30:51 +0300

    Fix Docker configuration and code style

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,18 +3,15 @@
 set -e
 
 echo "Waiting for database..."
-./wait-for-it.sh db:5432 --timeout=30 --strict -- echo "Database is up!"
+./wait-for-it.sh $DB_HOST:$DB_PORT --timeout=30 --strict -- echo "Database is up!"
 
-if [ "$1" = "web" ]; then
-    echo "Running migrations..."
-    python manage.py migrate movies
-    python manage.py migrate
+echo "Running migrations..."
+python manage.py migrate
+
+if [ "$1" = "celery" ]; then
+    echo "Starting celery worker..."
+    exec celery -A imdb_clone worker --loglevel=info
+else
     echo "Starting web server..."
     exec python manage.py runserver 0.0.0.0:8000
-elif [ "$1" = "celery" ]; then
-    echo "Starting celery worker..."
-    exec celery -A imdb_clone.celery.app worker --loglevel=info
-else
-    echo "Invalid argument provided. Use 'web' or 'celery'."
-    exit 1
 fi
