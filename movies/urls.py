90fd9d8ca6f8c4b440adf4568a84e2036e06fd50
onurmacit <onurmaciit@gmail.com>
2025-02-23 23:31:31 +0300

    Add paths

--- movies/urls.py
+++ movies/urls.py
@@ -4,10 +4,12 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from .views import RegisterView
+from .views import MovieDetailView, MovieListView, RegisterView
 
 urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/register/", RegisterView.as_view(), name="register"),
+    path("api/movie_list", MovieDetailView.as_view(), name="movie-list"),
+    path("api/movie_detail", MovieListView.as_view(), name="movie-detail"),
 ]
