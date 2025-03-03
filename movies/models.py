196d5bedc6dbe9b4ef324f6762fe7ed22a1b3168
onurmacit <onurmaciit@gmail.com>
2025-03-03 23:56:14 +0300

    added Category model with name and cover_url fields

--- movies/models.py
+++ movies/models.py
@@ -1,7 +1,7 @@
-from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.core.exceptions import ValidationError
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 class CustomUserManager(BaseUserManager):
@@ -57,14 +57,11 @@ class Movie(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    cover_image = CloudinaryField(
-        "image",
-        blank=True,
-        null=True,
-        folder="categories/",
-        default="default-category-cover.jpg",
-    )
+    name = models.CharField(max_length=255)
+    cover_url = CloudinaryField('category_covers', null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
