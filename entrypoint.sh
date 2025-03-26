a29fc803f4b5819390306f804b84807860e7e044
onurmacit <onurmaciit@gmail.com>
2025-03-26 23:37:09 +0300

    separate migration and Celery worker execution in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,16 +5,16 @@ set -e
 echo "Waiting for database..."
 ./wait-for-it.sh $DB_HOST:$DB_PORT --timeout=30 --strict -- echo "Database is up!"
 
-echo "Creating migrations..."
-python manage.py makemigrations movies
-
-echo "Running migrations..."
-python manage.py migrate
-
 if [ "$1" = "celery" ]; then
     echo "Starting celery worker..."
     exec celery -A imdb_clone worker --loglevel=info
 else
+    echo "Creating migrations..."
+    python manage.py makemigrations movies
+
+    echo "Running migrations..."
+    python manage.py migrate
+
     echo "Starting web server..."
     exec python manage.py runserver 0.0.0.0:8000
 fi
